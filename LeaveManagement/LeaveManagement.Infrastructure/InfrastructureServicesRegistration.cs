3be293c5ee961306cd50649867f3bc6d0884dad8
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-12-28 11:30:46 +0200

    Create Infrastructure project

---
+++ LeaveManagement/LeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,12 @@
+﻿namespace LeaveManagement.Infrastructure;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class InfrastructureServicesRegistration
+{
+    public static IServiceCollection ConfigureInfrastructureServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+        => services;
+}
